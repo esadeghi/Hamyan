9ff1b95e8c64a07a0da83afc86996b86b649ef05
Ehsan Sadeghi <esadeghi@gmail.com>
2025-05-09 09:54:32 -0400

    Add pricing component

---
+++ src/app/pricing/pricing.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+
+@Component({
+  selector: 'app-pricing',
+  imports: [CommonModule, TranslateModule],
+  templateUrl: './pricing.component.html',
+  styleUrl: './pricing.component.css'
+})
+export class PricingComponent {
+
+}
