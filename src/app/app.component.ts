e60b535e03a590424c55e86b4b2f6d3072dc76bc
Ehsan Sadeghi <esadeghi@gmail.com>
2025-05-13 16:42:49 -0400

    Add RouterOutlet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,9 +4,9 @@ import { LandingPageComponent } from './landing-page/landing-page.component';
 
 @Component({
   selector: 'app-root',
-  imports: [LandingPageComponent],
+  imports: [RouterOutlet, LandingPageComponent],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'hamyan';
