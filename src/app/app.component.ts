1348e2cfddc30a85639a709fe2d43b1c1f2347b2
Ehsan Sadeghi <esadeghi@gmail.com>
2025-05-14 11:33:21 -0400

    Add navigation to login page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LandingPageComponent } from './landing-page/landing-page.component';
+import { LanguageSelectorComponent } from './language-selector/language-selector.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LandingPageComponent],
+  standalone: true,
+  imports: [RouterOutlet, LanguageSelectorComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
