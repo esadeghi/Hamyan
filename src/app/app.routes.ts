7af013b6e1cbaa1c5f92273054ab233bbe9150ea
Ehsan Sadeghi <esadeghi@gmail.com>
2025-05-14 11:30:21 -0400

    Add project routes to login and homepage

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
-import { AppComponent } from './app.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 export const routes: Routes = [
-  { path: '', component: AppComponent },
+  { path: '', component: LandingPageComponent },
   { path: 'login', component: LoginComponent },
 ];
